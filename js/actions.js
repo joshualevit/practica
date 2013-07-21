8d02d81fbf40696fdd08cd58a235eac6291dfea9
Josue Aguilera <joshualevit@gmail.com>
2013-07-20 19:05:57 -0500

    2
    2

--- js/actions.js
+++ js/actions.js
@@ -3,30 +3,30 @@ var jQT = new $.jQTouch({
 	themeSelectionSelector: '#jqt'
 });
 
-$(document).ready(function() {
-	var watchID=null;
-	//Acelerometro
-	$('#acelerometro .individual li').tap(function(){
-			if($(this).index()==0){ //Boton de iniciar
-				function onSuccess(acceleration) {
-		$('#acelerometro h2').hmtl('Acceleration X: ' + acceleration.x + '<br>' +
-			'Acceleration Y: ' + acceleration.y + '<br>' +
-			'Acceleration Z: ' + acceleration.z + '<br>');
-};
-
-function onError() {
-    alert('onError!');
-};
-
-var options = { frequency: 500 };  // Update every 3 seconds
-
-watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
-			}else{ // Boton Detener
-				if (watchID) {
-            	navigator.accelerometer.clearWatch(watchID);
-            	watchID = null;
-					$('#acelerometro h2').hmtl('Detenido');
-        		}					
-			 }
-	});
+$(function(){
+    var watchID=null;
+    //Acelerometro
+    $('#acelerometro .individual li').tap(function(){
+        if($(this).index()==0){//Boton iniciar
+            function onSuccess(acceleration) {
+                $('#acelerometro h2').html('Acceleration X: ' + acceleration.x + '<br>' +
+                      'Acceleration Y: ' + acceleration.y + '<br>' +
+                      'Acceleration Z: ' + acceleration.z + '<br>');
+            };
+            
+            function onError() {
+                alert('onError!');
+            };
+            
+            var options = { frequency: 500 };  // Update every 3 seconds
+            
+            watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+        }else{//Boton Detener
+            if (watchID) {
+                navigator.accelerometer.clearWatch(watchID);
+                watchID = null;
+                $('#acelerometro h2').html('Detenido');
+            }
+        }
+    });
 });
